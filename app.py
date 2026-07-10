ae9a8f810228340c1bbed947edb2482ccb4f3511
efygj1 <efygj1@nottingham.ac.uk>
2026-07-10 20:40:50 +0100

    handling adding student data

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+students_list = []
+
 
 @app.route("/")
 def index():
@@ -10,10 +13,26 @@ def index():
 def students():
     return render_template("students.html")
 
-@app.route("/students/add")
+@app.route("/students/add", methods=["GET", "POST"])
 def add_student():
-    return render_template("add_student.html")
+    if request.method == 'POST':
+        name = request.form["name"]
+        year_group = request.form["year_group"]
+        exam_board = request.form["exam_board"]
+        grade = request.form["grade"]
 
+        student = {
+            "name": name,
+            "year_group": year_group,
+            "exam_board": exam_board,
+            "grade": grade
+        }
+
+        students_list.append(student)
+
+        print(students_list)
+
+    return render_template("add_student.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
